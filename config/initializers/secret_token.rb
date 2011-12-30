970f40fcec8b1de41d26984d7bb004959f8c029a
karima el arouach <soka_koko@hotmail.com>
2011-12-30 10:01:10 +0000

    depot ionitial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site1::Application.config.secret_token = 'fa993c3de5ad2b86cc2b20797bb4ab5406618e59132fc16467f4b3ea73f4b3916f36467c2c43ed4e0c3e3fb16365fde477ade24b9cb0c7b27c0e5afef8a34e50'
